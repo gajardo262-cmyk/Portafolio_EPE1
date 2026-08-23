b9c9ed6b2307dd6bea81bcf1a52a07f0f5107e5d
Roberto Gajardo <gajardo262@gmail.com>
2026-08-23 15:04:13 -0400

    JS Proyectos educativos

---
+++ js/data.js
@@ -0,0 +1,35 @@
+/* Proyectos Educativos */
+
+const PROYECTOS = [
+  {
+    id: 1,
+    nombre: "Sistema de Gestión y Simulación de Remuneraciones",
+    descripcion:
+      "Aplicación web desarrollada para gestionar funcionarios, liquidaciones y simulaciones de remuneraciones.",
+    stack: ["React", "Vite", "Redux", "JavaScript"],
+  },
+
+  {
+    id: 2,
+    nombre: "Aplicación de Realidad Aumentada",
+    descripcion:
+      "Aplicación móvil desarrollada para visualizar información digital mediante marcadores de realidad aumentada.",
+    stack: ["Unity", "Vuforia", "C#"],
+  },
+
+  {
+    id: 3,
+    nombre: "Sistema de Gestión de Ventas",
+    descripcion:
+      "Aplicación de escritorio desarrollada para gestionar ventas, clientes, productos y sucursales mediante una base de datos.",
+    stack: ["C#", "Windows Forms", "SQL Server"],
+  },
+
+  {
+    id: 4,
+    nombre: "Base de Datos Hospital Regional",
+    descripcion:
+      "Diseño de una base de datos hospitalaria, incluyendo modelado de información y estrategias de respaldo y recuperación.",
+    stack: ["SQL Server", "Base de Datos", "Respaldos"],
+  },
+];
